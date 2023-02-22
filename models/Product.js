fcec0f80a391181f498c18aced8f59660791fb8c
Prince <vishwakarmaprince833@gmail.com>
2023-02-22 23:17:29 +0530

    added filter

---
+++ models/Product.js
@@ -0,0 +1,13 @@
+const {model, models,  Schema } = require("mongoose");
+
+const ProductSchema = new Schema(  {
+    name: String,
+    description: String,
+    price: Number,
+    category: String, 
+    picture: String
+});
+
+
+const Product = models?.Product || model( 'Product',ProductSchema);
+export default Product;
